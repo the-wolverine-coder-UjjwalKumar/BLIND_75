fb7a14faffea593647b9e8c5bf5acfd79f97fd00
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-08-16 07:57:33 +0530

    Added TwoSum_01

--- blind75/src/main/java/array/TwoSum_LC_1_01.java
+++ blind75/src/main/java/array/TwoSum_LC_1_01.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum_01 {
+public class TwoSum_LC_1_01 {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
