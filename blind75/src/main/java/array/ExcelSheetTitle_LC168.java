2ae0a2480f573662f9b1a0cc92546faf620b71ce
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-08-23 07:53:06 +0530

    Added ExcelSheetTitle_LC168

---
+++ blind75/src/main/java/array/ExcelSheetTitle_LC168.java
@@ -0,0 +1,24 @@
+package array;
+
+public class ExcelSheetTitle_LC168 {
+    public static void main(String[] args) {
+        int n = 28;
+        String ans = convertToTitle(n);
+        System.out.println(ans);
+    }
+
+    // added solution
+    public static String convertToTitle(int columnNumber) {
+        int n = columnNumber;
+
+        StringBuilder result = new StringBuilder();
+
+        while (n > 0) {
+            n--;
+            result.insert(0, (char) ('A' + n % 26));
+            n /= 26;
+        }
+
+        return result.toString();
+    }
+}
