02d6fb321eb915d53b584621042e5f4afcf8b4eb
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-09-04 08:40:51 +0530

    Added StackReverseUsing RC

---
+++ blind75/src/main/java/algorithm/SortStackUsingRC.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+import java.util.Stack;
+
+public class SortStackUsingRC {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(2);
+        stack.push(1);
+        stack.push(3);
+        stack.push(4);
+        stack.push(0);
+        stack.push(5);
+
+        // sort the stack using recursion
+        System.out.println("Stack before sorting ; "+stack);
+        sortStack(stack);
+        System.out.println("Stack after sorting :"+stack);
+
+    }
+
+    private static void sortStack(Stack<Integer> stack) {
+
+        if (stack.isEmpty() || stack.size()==1) {
+            return;
+        }
+
+        int currentElement = stack.pop();
+        sortStack(stack);
+        // insert the currentElement in correct sorted position
+        insertIntoStack(stack, currentElement);
+
+    }
+
+    private static void insertIntoStack(Stack<Integer> stack, int currentElement) {
+        if (stack.isEmpty() || stack.peek() <= currentElement) {
+            stack.push(currentElement);
+            return;
+        }
+
+        int currentTopElement = stack.pop();
+        insertIntoStack(stack, currentElement);
+        stack.push(currentTopElement);
+    }
+}
