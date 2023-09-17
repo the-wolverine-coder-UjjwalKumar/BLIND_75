21288c6edb4ef7974738d675a6a620d6e2676e26
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-09-17 20:24:33 +0530

    added ClimbingStairs_LC70

---
+++ blind75/src/main/java/striver_atoz_dsa/dp/one_d/ClimbingStairs_LC70.java
@@ -0,0 +1,72 @@
+package striver_atoz_dsa.dp.one_d;
+
+import java.util.Arrays;
+
+public class ClimbingStairs_LC70 {
+    public static void main(String[] args) {
+        int n = 5; // steps to climb
+        int ans = getNumOfWaysToClimb(n);
+        System.out.println(ans);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        int ans1 = getNumOfWaysToClimbMemoized(n, dp);
+        System.out.println(ans1);
+
+        int ans2 = getNumOfWaysToClimbMemoizedTwo(n, dp);
+        System.out.println(ans2);
+
+        int ans3 = getNumOfWaysToClimbTopDown(n);
+        System.out.println(ans3);
+
+    }
+
+    // RC code
+    private static int getNumOfWaysToClimb(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+
+        return getNumOfWaysToClimb(n-1)+getNumOfWaysToClimb(n-2);
+    }
+
+    // DP memoized code
+    private static int getNumOfWaysToClimbMemoized(int n, int[] dp) {
+        if (n == 0 || n == 1) return dp[n];
+
+        if (dp[n]!= -1) return dp[n];
+
+        int ans = getNumOfWaysToClimbMemoized(n-1, dp) + getNumOfWaysToClimbMemoized(n-2, dp);
+        dp[n] = ans;
+        return dp[n];
+    }
+
+    private static int getNumOfWaysToClimbMemoizedTwo(int n, int[] dp) {
+        if (n == 0 || n == 1) return dp[n];
+
+        if (dp[n]!= -1) return dp[n];
+
+        int ans = dp[n-1] + dp[n-2];
+        dp[n] = ans;
+        return dp[n];
+    }
+
+    // DP top-down
+    private static int getNumOfWaysToClimbTopDown(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+
+    }
+
+
+
+
+}
