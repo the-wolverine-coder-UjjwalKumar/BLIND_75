e32c0a7a1cddd7e81b7dca6ab21b0a41189aca0b
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-09-18 10:07:26 +0530

    added MaximumHeightOfTree_LC104

---
+++ blind75/src/main/java/striver_atoz_dsa/tree/MaximumHeightOfTree_LC104.java
@@ -0,0 +1,23 @@
+package striver_atoz_dsa.tree;
+
+public class MaximumHeightOfTree_LC104 {
+    public static void main(String[] args) {
+        //       1
+        //      / \
+        //    2     3
+        //   / \   / \
+        //  4   5 7   8
+        //     /     / \
+        //     6    9  10
+        Node root = DFS_Traversal.getTree();
+
+        int height = getHeight(root);
+        System.out.println(height);
+    }
+
+    private static int getHeight(Node root) {
+        if (root == null) return 0;
+
+       return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+}
