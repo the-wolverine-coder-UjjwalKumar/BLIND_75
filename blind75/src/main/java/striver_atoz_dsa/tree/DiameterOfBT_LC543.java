a25337163ec72b0b44fa9ba2e49631d929ad6df3
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-09-20 10:01:41 +0530

    added DiameterOfBT_LC543

---
+++ blind75/src/main/java/striver_atoz_dsa/tree/DiameterOfBT_LC543.java
@@ -0,0 +1,38 @@
+package striver_atoz_dsa.tree;
+
+public class DiameterOfBT_LC543 {
+    public static void main(String[] args) {
+        //       1
+        //      / \
+        //    2     3
+        //   / \   / \
+        //  4   5 7   8
+        //     /     / \
+        //     6    9  10
+        Node root = DFS_Traversal.getTree();
+        // this will not work bcz it is creating new integer and trying to reassign it inside the method
+        // which will not affect the old reference, hence we won't find any changes
+//        Integer num = 5;  // Create an Integer object with value 5
+//
+//        // Attempting to modify the value of num
+//        num = num + 1;    // Creates a new Integer object with value 6
+//
+//        System.out.println(num);  // Outputs 6
+//        Integer diameter = new Integer(0);
+
+        // so use mutable ds
+        int[] diameter = new int[]{0};
+        int height = getDiameterOfTree(root, diameter);
+        System.out.println(diameter[0]);
+    }
+
+    private static int getDiameterOfTree(Node root, int[] diameter) {
+        if (root == null) return 0;
+        int lh = getDiameterOfTree(root.left, diameter);
+        int rh = getDiameterOfTree(root.right, diameter);
+
+        diameter[0] = Math.max(diameter[0], lh + rh);
+
+        return 1 + Math.max(lh, rh);
+    }
+}
