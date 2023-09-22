a62f89f86388f9782622d499fe24ed62d530e619
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-09-22 09:22:09 +0530

    added MaximumPathSum_LC124

---
+++ blind75/src/main/java/striver_atoz_dsa/tree/MaximumPathSum_LC124.java
@@ -0,0 +1,29 @@
+package striver_atoz_dsa.tree;
+
+public class MaximumPathSum_LC124 {
+
+    public static void main(String[] args) {
+        //       1
+        //      / \
+        //    2     3
+        //   / \   / \
+        //  4   5 7   8
+        //     /     / \
+        //     6    9  10
+        TreeNode root = DFS_Traversal.getTree();
+        int[] maxi = {0};
+        int ans = getMaxPathSum(root, maxi);
+        System.out.println(maxi[0]);
+    }
+
+    private static int getMaxPathSum(TreeNode root, int[] maxi) {
+        if (root == null) return 0;
+
+        int leftPathSum = Math.max(0,getMaxPathSum(root.left, maxi));
+        int rightPathSum = Math.max(0,getMaxPathSum(root.right, maxi));
+
+        maxi[0] = Math.max(maxi[0], root.val + leftPathSum + rightPathSum);
+
+        return root.val + Math.max(leftPathSum, rightPathSum);
+    }
+}
