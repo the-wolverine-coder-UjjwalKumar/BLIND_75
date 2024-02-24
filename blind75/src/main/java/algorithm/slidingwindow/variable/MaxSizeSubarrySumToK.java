6a7fa1f42608626fd6ccbf5046b414638405c55b
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-02-24 23:11:16 +0530

    updated MaxSizeSubarrySumToK

--- blind75/src/main/java/algorithm/slidingwindow/variable/MaxSizeSubarrySumToK.java
+++ blind75/src/main/java/algorithm/slidingwindow/variable/MaxSizeSubarrySumToK.java
@@ -1,9 +1,16 @@
 package algorithm.slidingwindow.variable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaxSizeSubarrySumToK {
     public static void main(String[] args) {
         int[] arr = {4,1,1,1,2,3,5};
         int k = 5;
+
+        int[] arr1 = {4,1,1,-2,1,3,5};
+        int k1 = 5;
+
 
         // return the max length of subarray whose sum equals to K
         int ans = getSubarraySumK(arr, k);
@@ -23,12 +30,38 @@ public class MaxSizeSubarrySumToK {
             if (sum == k) {
                 ans = Math.max(ans, j-i+1);
             } else if (sum > k) {
-                sum -= arr[i];
-                i++;
+                while (sum > k && i < arr.length) {
+                    sum -= arr[i];
+                    i++;
+                }
             }
 
             j++;
+
         }
         return ans;
+    }
+
+    // handle negative cases
+    public static int maxSubarraySum(int[] nums, int k) {
+        int maxLen = 0;
+        int currSum = 0;
+        Map<Integer, Integer> sumMap = new HashMap<>();
+        sumMap.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+
+            if (sumMap.containsKey(currSum - k)) {
+                maxLen = Math.max(maxLen, i - sumMap.get(currSum - k));
+            }
+
+            // Store the first occurrence of the current cumulative sum
+            if (!sumMap.containsKey(currSum)) {
+                sumMap.put(currSum, i);
+            }
+        }
+
+        return maxLen;
     }
 }
