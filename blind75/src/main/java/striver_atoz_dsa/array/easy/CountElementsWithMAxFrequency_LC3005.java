560dc0f20409becfa1d97e3879a3e26ad6772e3f
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-03-08 21:36:04 +0530

    added CountElementsWithMAxFrequency_LC3005

--- blind75/src/main/java/striver_atoz_dsa/array/easy/CountElementsWithMAxFrequency_LC3005.java
+++ blind75/src/main/java/striver_atoz_dsa/array/easy/CountElementsWithMAxFrequency_LC3005.java
@@ -4,23 +4,23 @@ import java.util.*;
 
 public class CountElementsWithMAxFrequency_LC3005 {
     public static void main(String[] args) {
-        int[] arr = {1,2,2,3,1,4};
-        int[] arr1 = {1,2,3,4,5};
-        int[] arr2 = {10,12,11,9,6,19,11};
+        int[] arr = {1, 2, 2, 3, 1, 4};
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {10, 12, 11, 9, 6, 19, 11};
         int ans = maxFrequencyElements(arr2);
         System.out.println(ans);
     }
 
     public static int maxFrequencyElements1(int[] arr) {
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i : arr) {
             int count = map.getOrDefault(i, 0);
-            map.put(i, count+1);
+            map.put(i, count + 1);
         }
 
         int maxCount = -1;
         int elementsCount = 0;
-        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
 
             int val = entry.getValue();
             if (maxCount == -1 || val > maxCount) {
@@ -28,7 +28,7 @@ public class CountElementsWithMAxFrequency_LC3005 {
             }
 
             if (val == maxCount) {
-                elementsCount+=val;
+                elementsCount += val;
             }
 
         }
@@ -38,22 +38,18 @@ public class CountElementsWithMAxFrequency_LC3005 {
     }
 
     public static int maxFrequencyElements(int[] nums) {
-        int a[]=new int[101];
-        for(int i:nums)
-        {
+        int a[] = new int[101];
+        for (int i : nums) {
             a[i]++;
         }
-        int maxi=0;
-        for(int i=0;i<=100;i++)
-        {
-            maxi=Math.max(maxi,a[i]);
+        int maxi = 0;
+        for (int i = 0; i <= 100; i++) {
+            maxi = Math.max(maxi, a[i]);
         }
-        int ans=0;
-        for(int i=0;i<=100;i++)
-        {
-            if(a[i]==maxi)
-            {
-                ans+=maxi;
+        int ans = 0;
+        for (int i = 0; i <= 100; i++) {
+            if (a[i] == maxi) {
+                ans += maxi;
             }
         }
         return ans;
