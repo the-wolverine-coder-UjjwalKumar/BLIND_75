2cf122a3514870c6fe39ec2761b4f66218f50bfe
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-03-29 20:18:12 +0530

    added CountSubarrayWithAtLeastKMaxElements_LC2962

--- blind75/src/main/java/algorithm/slidingwindow/variable/CountSubarrayWithAtLeastKMaxElements_LC2962.java
+++ blind75/src/main/java/algorithm/slidingwindow/variable/CountSubarrayWithAtLeastKMaxElements_LC2962.java
@@ -58,4 +58,23 @@ public class CountSubarrayWithAtLeastKMaxElements_LC2962 {
         return ans;
     }
 
+    public static long countSubarrays2(int[] nums, int k) {
+        long res = 0;
+        int max = 0, count = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] == max)
+                count++;
+            while (count >= k) {
+                if (nums[l++] == max)
+                    count--;
+            }
+            res += l;
+        }
+        return res;
+    }
+
 }
